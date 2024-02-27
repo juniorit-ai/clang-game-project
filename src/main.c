6551ad78088c16c31cd96ccfff05961bf04703b8
ikaushalvir <kaushaljainer@gmail.com>
2024-02-28 00:50:59 +0530

    add a multiline comment to main.c

--- src/main.c
+++ src/main.c
@@ -2,24 +2,35 @@
 #include "FirstScene.h"
 #include <stdio.h>
 
+/*
+   The following code initializes a basic JuniorIT GameCraft application.
+   It includes necessary headers, sets up the game parameters, creates and adds
+   the first scene, and runs the game loop.
+*/
+
 int main(int argc, char *argv[])
 {
+    // Initializing the name, width, and height
     const char* name = "JuniorIT GameCraft App";
-
     int width = 1280;
     int height = 512;
 
+    // Printing the start message with application details
     printf("%s starts; Scene width: %i, height: %i.\n", name, width, height);
 
+    // Initializing the game and setting up the first scene
     Game *game = game_instance();
     game->init(game, name, width, height);
 
+    // Creating and adding the first scene to the game
     Scene *scene = (Scene *)new_first_scene();
     game->add_scene(game, scene);
     game->set_current_scene(game, scene);
 
+    // Running the game
     game->run(game);
 
+    // Cleaning up and destroying the game
     game->destroy(game);
 
     return 0;
